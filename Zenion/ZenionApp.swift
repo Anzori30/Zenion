5165a76510a482c07766612c0f85703d6480070e
anzori <anzoriqiziyelashvili11@gmail.com>
2023-04-04 22:55:11 +0400

    Initial Commit

---
+++ Zenion/ZenionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenionApp.swift
+//  Zenion
+//
+//  Created by macbook on 04.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ZenionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
